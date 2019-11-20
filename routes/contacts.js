77314961129c3b8e01c0d3049ff9d4f7372c3d1d
Danny Beltran <dannyb@uci.edu>
2019-11-19 20:52:51 -0800

    initial commit. Express server with initial routers

---
+++ routes/contacts.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+//@route    GET    api/contacts
+//@desc     Get all users contacts
+//@access   Private
+router.get("/", (req, res) => {
+  res.send("get logged in user's contacts");
+});
+
+//@route    POST    api/contacts
+//@desc     Add new contact
+//@access   Private
+router.post("/", (req, res) => {
+  res.send("add contact");
+});
+
+//@route    PUT    api/contacts/:id
+//@desc     Update contact
+//@access   Private
+router.put("/:id", (req, res) => {
+  res.send("update contact");
+});
+
+//@route    Delete    api/contacts/:id
+//@desc     Auth user & get token
+//@access   Private
+router.delete("/:id", (req, res) => {
+  res.send("delete contact");
+});
+
+module.exports = router;
